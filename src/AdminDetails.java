07e5013d5d9b17d36b5de6bf3cb882b01a5c97ee
DEVISETTI SAI PAVAN KUMAR <devisettisaipavankumar@gmail.com>
2024-12-18 23:00:58 +0530

    Update AdminDetails.java

--- src/AdminDetails.java
+++ src/AdminDetails.java
@@ -1,10 +1,11 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 class AdminDetails {
-    private HashMap<String, Admin> adminMap;
+    private List<Admin> adminList;
 
     public AdminDetails() {
-        adminMap = new HashMap<>();
+        adminList = new ArrayList<>();
         // Add default admins
         registerAdmin("admin1", "password1");
         registerAdmin("admin2", "password2");
@@ -12,22 +13,23 @@ class AdminDetails {
 
     public void registerAdmin(String adminId, String adminPassword) {
         Admin admin = new Admin(adminId, adminPassword);
-        adminMap.put(adminId, admin);
+        adminList.add(admin);
     }
 
     public Admin loginAdmin(String adminId, String password) {
-        Admin admin = adminMap.get(adminId);
-        if (admin != null && admin.validateAdmin(adminId, password)) {
-            return admin;
+        for (Admin admin : adminList) {
+            if (admin.getAdminId().equals(adminId) && admin.validateAdmin(adminId, password)) {
+                return admin;
+            }
         }
         return null;
     }
 
-    public HashMap<String,Admin> getAdminMap() {
-        return adminMap;
+    public List<Admin> getAdminList() {
+        return adminList;
     }
 
-    public void setAdminMap(HashMap<String,Admin> adminMap) {
-        this.adminMap = adminMap;
+    public void setAdminList(List<Admin> adminList) {
+        this.adminList = adminList;
     }
 }
